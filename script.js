966c9b79d3d7307c79d3ca3ebce22d7aa10f23b8
Hareesh123NH <162862005+Hareesh123NH@users.noreply.github.com>
2024-10-08 19:40:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function createTable() {
     //Write your code here
-  
+	const table=document.getElementById("myTable");
+	let rn=prompt("Input number of rows");
+	let cn=prompt("Input number of columns");
+	for(let i=0;i<rn;i++){
+		const row=document.createElement("tr");
+		for(let j=0;j<cn;j++){
+			const col=document.createElement("td");
+			col.innerText=`Row-${i} Column-${j}`;
+			row.appendChild(col);
+		}
+		table.appendChild(row);
+	}
 }
